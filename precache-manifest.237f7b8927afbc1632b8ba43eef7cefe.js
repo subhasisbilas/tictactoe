27cf573587d550303241289316d0423711564a68
SubhasisBilas <subhasis.bilas@gmail.com>
2020-06-17 23:27:30 +0100

    Updates

---
+++ precache-manifest.237f7b8927afbc1632b8ba43eef7cefe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad21d4271fb1fbd39e237d75cc23ac0",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "06cd5a20a952f1b0dd7c",
+    "url": "/tictactoe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2f8ba9499ca3bd336610",
+    "url": "/tictactoe/static/js/2.3c44a1cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.3c44a1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06cd5a20a952f1b0dd7c",
+    "url": "/tictactoe/static/js/main.13a9fd73.chunk.js"
+  },
+  {
+    "revision": "8fd1009f88de2a8f7d1e",
+    "url": "/tictactoe/static/js/runtime-main.57baa19c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictactoe/static/media/logo.5d5d9eef.svg"
+  }
+]);
